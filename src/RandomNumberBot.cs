e6d97fe8300b3f94f9ee6a9da28f2339a7f7de7f
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-02-04 17:23:51 +0100

    Initial commit

---
+++ src/RandomNumberBot.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordRandomNumber
+{
+    class RandomNumberBot
+    {
+        private CommandService _commands;
+        private DiscordSocketClient _client;
+        private IServiceProvider _services;
+
+        static void Main() => new RandomNumberBot().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            // Add this for logging.
+            _client.Log += Log;
+
+            string token = ""; // Please keep this part private
+
+            _services = new ServiceCollection()
+                .BuildServiceProvider();
+
+            await InstallCommands();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            _client.MessageReceived += HandleCommand;
+
+            // Discover all of the commands in this assembly and load them.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+
+            // Create a Command Context
+            var context = new CommandContext(_client, message);
+
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+    }
+}
---
+++ src/RandomNumberModule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordRandomNumber
+{
+    public class RandomNumberModule : ModuleBase
+    {
+        private static Random Random = new Random();
+
+        // ~say hello -> hello
+        [Command("say"), Summary("Echos a message.")]
+        public async Task Say([Remainder, Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync(echo);
+        }
+
+        /// <summary>
+        /// Usage: random {lower limit} {upper limit}
+        /// </summary>
+        /// <returns>The generated random number</returns>
+        [Command("random"), Summary("Generates a random number between two numbers")]
+        public async Task GenerateRandomNumber([Summary("The lower limit")] int min, [Summary("The upper limit")] int max)
+        {
+            string generatedNumber = Random.Next(min, max).ToString();
+
+            // Return the generated random number.
+            await ReplyAsync(generatedNumber);
+        }
+    }
+
+    // Create a module with the 'sample' prefix
+    [Group("sample")]
+    public class Sample : ModuleBase
+    {
+        // ~sample square 20 -> 400
+        [Command("square"), Summary("Squares a number.")]
+        public async Task Square([Summary("The number to square.")] int num)
+        {
+            // We can also access the channel from the Command Context.
+            await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
+        }
+
+        // ~sample whois 96642168176807936 --> Khionu#8708
+        [Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfo([Summary("The (optional) user to get info for")] IUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    }
+}
